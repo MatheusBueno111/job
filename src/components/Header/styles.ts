74a51a5921a2cf82c648389f6f0aac51323bf65e
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2022-06-30 13:00:52 -0300

    FEAT: Adicionando responsividade

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,13 +3,11 @@ import styled from 'styled-components'
 export const HeaderContainer = styled.header`
   
   margin: 6.25rem auto 6.25rem;
-  width: 52.5rem;
   height: fit-content;
-  
+  width: 100%;
   text-align: center;
   color: #151515; 
   
-  width: 1000px;
   align-items: center;
 
   
