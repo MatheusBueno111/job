370902d6f0364211c938842d9c75da857db36b82
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2022-06-30 14:03:49 -0300

    FIX: answer na optionFAQ

--- src/components/SimpleAccordion/optionFAQ.tsx
+++ src/components/SimpleAccordion/optionFAQ.tsx
@@ -5,7 +5,7 @@ export const optionFAQ = [
     ' Fiz basquete dos 7 ao 15 anos, aprendi muito sobre o valor do trabalho em equipe, competitividade com outros e comigo mesmo' +
     ', sempre buscando a minha melhor versão. Gosto de ouvir música, andar de bicicleta e fazer trilhas sozinho mesmo, ter um momento só meu.' +
     ' Aos 11 anos, tive o meu primeiros contato com programação e robôs, em um programa do Sesi, o qual me fez despertar interesse sobre o assunto.' +
-    'Fiz ensino Médio/Técnico, de forma que me deu oportunidade de estagiar como técnico em eletrônica na Embraer e dessa forma fui aprendendo como é profissional'+
+    'Fiz ensino Médio/Técnico, de forma que me deu oportunidade de estagiar como técnico em eletrônica na Embraer e dessa forma fui aprendendo como é ser profissional'+
     ' e como isso impacta o pessoal. Ao longo dos anos, me dediquei aos estudos para entrar na universidade que eu sempre quis e '+
     'nela descobri o que realmente quero fazer da minha carreira profissional.'
   },
@@ -17,14 +17,14 @@ export const optionFAQ = [
   },
   {
     question:'Por que você seria brilhante no nosso time?',
-    answer: 'Sou ótima pessoa para trabalhar em equipe (devido ao meu background no esporte e no estágio) porém sei trabalhar sozinho também. ' +
+    answer: 'Sou ótima pessoa para trabalhar em equipe (devido ao meu background no esporte e no estágio), porém sei trabalhar sozinho também. ' +
     'Além disso, estaria dando um passo mais próximo do meu sonho e eu farei de tudo para conseguir realizá-lo.'
   },
   {
     question:'O que você já fez de diferente na faculdade e na vida? E que resultados conquistou por seu próprio mérito?',
     answer:'Entrei em um projeto universitário, a Robok, que atua na área de competição de futebol de robôs autônomos. Participei da equipe de software,' +
     ' onde analisávamos os pontos a serem atingidos por metas, consultávamos professores, fazíamos pesquisas sobre materiais a serem adquiridos,' +
-    ' com a finalidade de fazer o robô funcionar. No estágio que eu fiz antes de entrar na universidade adquiri muito conhecimento e aprendi muito sobre a vida,'+
+    ' com a finalidade de fazer um robô competitivo. No estágio que eu fiz antes de entrar na universidade adquiri muito conhecimento e aprendi muito sobre a vida,'+
     ' porque trabalhei com pessoas que puderam compartilhar suas experiências e a maior lição foi sempre seguir focado em quem eu quero ser. O que serviu de base para mim,' +
     ' onde tinha 18 anos e estava entrando na universidade que tanto quis. Isso foi uma conquista e valeu a pena todo esforço.'+
     ' Entretanto, sempre tive pessoas próximas a mim que me ajudaram durante a jornada como minha família.'
@@ -32,7 +32,7 @@ export const optionFAQ = [
   {
     question:'Explique seus pontos fortes e fracos, citando exemplos de situações reais que já viveu.',
     answer:'Sou muito determinado, vou atrás do que eu quero porque eu sei o que quero, trabalho muito bem em equipe, sou muito adaptável.' +
-    ' Eu não gosto de falar que eu tenho pontos fracos, prefiro falar que eu tenho oportunidades de crescimento em ser muito detalhista, sou muito curioso' +
+    ' Eu não gosto de falar que eu tenho pontos fracos, prefiro falar que eu tenho oportunidades de crescimento em ser muito detalhista e sou muito curioso' +
     'sempre gosto de estudar e aprender coisas novas, foi desse forma que começei a estudar programação e desenvolvimento web, por conta própria.'
   },
   {
