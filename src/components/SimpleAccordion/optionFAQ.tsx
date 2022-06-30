9eca10772ed03480c51676d4fe1b729d082c0369
Matheus Bueno <matheus.bueno.queiroz@gmail.com>
2022-06-30 13:33:30 -0300

    FIX: answer na optionFAQ

--- src/components/SimpleAccordion/optionFAQ.tsx
+++ src/components/SimpleAccordion/optionFAQ.tsx
@@ -2,7 +2,7 @@ export const optionFAQ = [
   {
     question:'Quem é você e porque quer trabalhar na Inova?',
     answer: 'Meu nome é Matheus Bueno Queiroz, nasci em São José dos Campos, tenho 23 anos, curso engenharia da computação na UNIFEI de Itajubá.' +
-    'Fiz basquete dos 7 ao 15 anos, aprendi muito sobre o valor do trabalho em equipe, competitividade com outros e comigo mesmo' +
+    ' Fiz basquete dos 7 ao 15 anos, aprendi muito sobre o valor do trabalho em equipe, competitividade com outros e comigo mesmo' +
     ', sempre buscando a minha melhor versão. Gosto de ouvir música, andar de bicicleta e fazer trilhas sozinho mesmo, ter um momento só meu.' +
     ' Aos 11 anos, tive o meu primeiros contato com programação e robôs, em um programa do Sesi, o qual me fez despertar interesse sobre o assunto.' +
     'Fiz ensino Médio/Técnico, de forma que me deu oportunidade de estagiar como técnico em eletrônica na Embraer e dessa forma fui aprendendo como é profissional'+
